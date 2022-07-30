2187db72309af8948b11a4a9f50cdc3d610d383f
Shenal Perera <e17246@eng.pdn.ac.lk>
2022-07-30 20:09:30 +0530

    Binary search tree

---
+++ C/Clients/humaned/Code/cmake-build-debug/CMakeFiles/Code.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code.dir/main.c.o"
+  "Code.exe"
+  "Code.pdb"
+  "libCode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C/Clients/humaned/Code/test.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdio.h>
+
+int main(){
+
+    char name1[] = "zbcd";
+    char name2[] = "bcde";
+
+    printf("%d", strcmp(name1,name2));
+    return 0;
+}
+
